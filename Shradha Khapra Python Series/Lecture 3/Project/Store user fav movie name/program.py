f56efbd302c89633c810e9326c3852ce2b6a628a
sumitaher-byte <sumitaher2645@gmail.com>
2026-08-19 16:03:01 +0530

    Solve ex of storing user 3 movie name into list

--- Shradha Khapra Python Series/Lecture 3/Project/Store user fav movie name/program.py
+++ Shradha Khapra Python Series/Lecture 3/Project/Store user fav movie name/program.py
@@ -1,1 +1,7 @@
-# just to check
+m1 = str(input("Enter your 1st fav movie name:-"))
+m2 = str(input("Enter your 2nd fav movie name:-"))
+m3 = str(input("Enter your 3rd fav movie name:-"))
+
+movie_list = [m1,m2,m3]
+
+print(movie_list)
