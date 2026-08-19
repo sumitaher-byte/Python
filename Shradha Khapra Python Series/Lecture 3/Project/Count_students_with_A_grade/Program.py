126d7456535b71f184b471f9bb40e9494b29e0b3
sumitaher-byte <sumitaher2645@gmail.com>
2026-08-19 16:44:15 +0530

    1) Write program to count student with A Count_students_with_A_grade. 2) Write program to take input make list and sort it.

---
+++ Shradha Khapra Python Series/Lecture 3/Project/Count_students_with_A_grade/Program.py
@@ -0,0 +1,6 @@
+#WAP to count the no of students with a grade in folllowing tuple.
+# (C,D,A,A,B,B,A)
+
+tup = ('C','D','A','A','B','B','A')
+
+print(tup.count('A'))
---
+++ Shradha Khapra Python Series/Lecture 3/Project/Store_values_in_list_sort_them/program.py
@@ -0,0 +1,14 @@
+#store above values in the list and sort them from A to D
+
+list = []
+
+list.append(str(input("Enter:-")))
+list.append(str(input("Enter:-")))
+list.append(str(input("Enter:-")))
+list.append(str(input("Enter:-")))
+list.append(str(input("Enter:-")))
+list.append(str(input("Enter:-")))
+list.append(str(input("Enter:-")))
+
+list.sort()
+print(list)
